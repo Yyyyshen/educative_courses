35d390f087f5bb34aa73c66f48d1f6e616ec3f43
Yyyyshen <Yyyyshen@gmail.com>
2021-03-08 17:23:01 +0800

    添加项目文件。

---
+++ educative_courses/educative_courses.cpp
@@ -0,0 +1,18 @@
+﻿// educative_courses.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 之前计划开始学算法看到了公众号推荐的教育网站
+ * https://www.educative.io/
+ * 脑抽直接买了一年的订阅
+ * 后来发现看完需要的课程也就一个月就够了
+ * 干脆把站里其他觉得有用的课都看看
+ */
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
